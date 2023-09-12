76a95907c6c7e1cb70c5f940fef3782e610553df
swijethunga <95966574+swijethunga@users.noreply.github.com>
2023-09-12 18:42:01 +0530

    finalized user management

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { Users, removedUsers } = require("../models");
 const { Op } = require("sequelize");
+require("dotenv").config();
 
 //get all user details
 const getAllUsers = async (req, res) => {
@@ -31,7 +32,6 @@ const getUser = async (req, res) => {
 const createUser = async (req, res) => {
   const { FirstName, LastName, Email, PhoneNumber } = req.body;
   try {
-    //check if credentials are missing
     if (!FirstName || !LastName || !Email || !PhoneNumber)
       return res.status(404).json({ error: "Missing Credentials" });
 
@@ -62,7 +62,7 @@ const createUser = async (req, res) => {
     });
 
     if (existingRemovedUser) {
-      // Remove the user from RemovedUsers if found 
+      // Remove the user from RemovedUsers if found
       await removedUsers.destroy({
         where: {
           [Op.or]: [{ Email }, { PhoneNumber }],
@@ -88,6 +88,8 @@ const updateUser = async (req, res) => {
   const { userId, FirstName, LastName, Email, PhoneNumber } = req.body;
 
   try {
+    if (!userId) return res.status(404).json({ error: "user Id is required" });
+
     // Check if the user exists
     const user = await Users.findOne({
       where: { userId },
@@ -104,7 +106,9 @@ const updateUser = async (req, res) => {
       });
 
       if (existingEmail) {
-        return res.status(400).json({ error: "Email already registered" });
+        return res
+          .status(400)
+          .json({ error: "Email already registered by another user" });
       }
     }
 
@@ -116,7 +120,7 @@ const updateUser = async (req, res) => {
       if (existingPhone) {
         return res
           .status(400)
-          .json({ error: "Phone Number already registered" });
+          .json({ error: "Phone Number already registered by another user" });
       }
     }
 
@@ -147,15 +151,41 @@ const updateUser = async (req, res) => {
 //delete a user (move to removed User table)
 const deleteUser = async (req, res) => {
   const { userId } = req.body;
-  console.log(userId);
 
   try {
+    if (!userId) return res.status(404).json({ error: "User Id is required" });
+
     const user = await Users.findOne({
       where: { userId },
     });
 
     if (!user) {
       return res.status(404).json({ error: "User not found" });
+    }
+
+    // Check if the user has current orders
+    const response = await fetch(
+      `${process.env.ORDERS_API}/check/user/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (response.status !== 200) {
+      return res.status(response.status).json({ error: responseData.error });
+    }
+
+    const { hasCurrentOrders, orderIds } = responseData;
+
+    if (hasCurrentOrders) {
+      return res
+        .status(400)
+        .json({ error: "Failed! ,User has pending orders", orderIds });
     }
 
     const removedUserData = { ...user.toJSON() };
@@ -168,7 +198,7 @@ const deleteUser = async (req, res) => {
 
     return res.status(200).json({ message: "User removed successfully" });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
--- routes/Users.js
+++ routes/Users.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/get/all", getAllUsers);
 router.get("/get/:userId", getUser);
-router.post("/create/", createUser);
+router.post("/create", createUser);
 router.patch("/update", updateUser);
 router.delete("/delete", deleteUser);
 
